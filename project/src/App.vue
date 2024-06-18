1b344fa0fb01979fc3da37af2f3e5941a8521be4
XUX <1716824725@qq.com>
2024-06-18 16:09:40 +0800

    1、配置publicPath

--- project/src/App.vue
+++ project/src/App.vue
@@ -6,6 +6,10 @@
   <router-view />
 </template>
 
+<script setup>
+console.log("########", process.env);
+</script>
+
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- project/vue.config.js
+++ project/vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   transpileDependencies: true,
   productionSourceMap: false,
+  publicPath: process.env.BASE_URL,
   outputDir: "../website",
 });
